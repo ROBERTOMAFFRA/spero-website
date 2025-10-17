0dfb5ad892e45feea89db13805e6f979e8f6c2b2
ROBERTOMAFFRA <sperorestor@outlook.com>
2025-10-16 23:33:21 -0400

    Update form.js

--- static/js/form.js
+++ static/js/form.js
@@ -1,5 +1,6 @@
 document.getElementById('inspectionForm').addEventListener('submit', async (e) => {
   e.preventDefault();
+
   const form = e.target;
   const responseMsg = document.getElementById('formResponse');
   responseMsg.textContent = 'Sending...';
@@ -13,10 +14,7 @@ document.getElementById('inspectionForm').addEventListener('submit', async (e) =
 
   const result = await res.json();
   responseMsg.textContent = result.message;
-  if (result.success) {
-    form.reset();
-    responseMsg.style.color = 'green';
-  } else {
-    responseMsg.style.color = 'red';
-  }
+  responseMsg.style.color = result.success ? 'green' : 'red';
+
+  if (result.success) form.reset();
 });
